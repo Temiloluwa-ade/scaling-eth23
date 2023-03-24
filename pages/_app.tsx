a8e5a8643ae9463e157959ab82ebff32d28907b7
george-hub331 <idiaghegeorge9@gmail.com>
2023-03-24 21:24:27 +0100

    update: add payment context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,38 +4,19 @@ import "@rainbow-me/rainbowkit/styles.css";
 
 import type { AppProps } from "next/app";
 import React from "react";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   
   const once = React.useRef<boolean>(true);
   
 
-  // analytics.identify()
 
   return (
     <>
 
       <Component {...pageProps} />
 
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,
-          });
-`,
-        }}
-      />
+     
     </>
   );
 }
